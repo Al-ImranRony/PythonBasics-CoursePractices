6bb22d752a4dd0301746e241bb56e94934ea77f5
Al-ImranRony <imranrony687@gmail.com>
2022-10-19 23:58:01 +0600

    Temp commit, introducing numbers in python

--- Lesson2.py
+++ Lesson2.py
@@ -8,3 +8,9 @@ from cmath import pi
 
 
 print("1. Our first expression in python is: ", pi, "meters long.")
+
+print("2. Introducing numbers in python...")
+
+num = 10
+decimalNumber = 11.5
+print("Adding two different numbers with python -> ", "10 + 11.5 =", num + decimalNumber)
