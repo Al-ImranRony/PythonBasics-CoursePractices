bc87b277f89fa7c44b91415d28ffa5749329ee44
Al-ImranRony <imranrony687@gmail.com>
2022-10-29 00:19:24 +0600

    Lesson 3 examples

--- Lesson3.py
+++ Lesson3.py
@@ -2,3 +2,17 @@
     Conditional execution 
 '''
 
+x, y = 5, 10
+
+if x != y :
+    if(x < y):
+        print("x is smaller than y? Yes, that's true !")
+    elif(x > y):
+        print("x is greater than y? No, that's false !")
+    # elif x >= y:
+    #     print("x is greater or equal to y? No, that's also false !")
+    # else:           
+    #     print("x is equal to y? No, false !")
+else:
+    print("x is equal to y")
+
