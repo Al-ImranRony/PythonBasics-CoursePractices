7cc5454678a95218e23113d3b8b9cde411be189f
Al-ImranRony <imranrony687@gmail.com>
2022-10-29 21:50:44 +0600

    Solved first problem from hackerrank.

--- Lesson3.py
+++ Lesson3.py
@@ -2,17 +2,39 @@
     Conditional execution 
 '''
 
-x, y = 5, 10
 
-if x != y :
-    if(x < y):
-        print("x is smaller than y? Yes, that's true !")
-    elif(x > y):
-        print("x is greater than y? No, that's false !")
-    # elif x >= y:
-    #     print("x is greater or equal to y? No, that's also false !")
-    # else:           
-    #     print("x is equal to y? No, false !")
+x = 100
+y = 100
+
+
+if (x == y):
+    if (x > y):                             # Nested conditional execution.
+        print (" x is greater than y ")
+    
+    elif ( x >= y):
+        print(" x is greater or equal to y")
+
+    elif (x <= y):
+        print(" x is less or equal to y")
+    
+    else:
+        print (" x is less than y ")
+
 else:
-    print("x is equal to y")
+    print("X is not equal to Y.")
 
+# Example problem And solution from Hackerrank
+
+n = 8
+
+if (n % 2 != 0):                    # modulus operator
+    print("Weird   1")
+
+elif (n % 2 == 0 and n>2 and n<5):
+    print("Not weird  2")
+
+elif (n % 2 == 0 and n>6 and n<20):
+    print("Weird    3")
+
+elif (n % 2 == 0 and n > 20):
+    print("Not weird  4")
